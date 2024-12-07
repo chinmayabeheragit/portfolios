d59821deba8293edbcdee9952cb22e6c952aa480
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-07 20:45:09 +0530

    push after some changes

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -7,7 +7,6 @@ const certificateData = [
     { id: 2, name: 'Certificate 2', src: require('../assets/javascript certificates.png'), link: 'https://www.hackerrank.com/certificates/bcc077743be0' },
     { id: 3, name: 'Certificate 3', src: require('../assets/pyhtoncertificate.jpeg'), link: 'https://www.hackerrank.com/certificates/8d32ea1b3b5a' },
     { id: 4, name: 'Certificate 4', src: require('../assets/udemy js.jpg'), link: 'http://ude.my/UC-5def4e8d-c389-4652-a13e-fa9c8ac90004' },
-
     // Add more certificates here
 ];
 
@@ -36,21 +35,26 @@ const Certification = () => {
     return (
         <section className="certification-container">
             <h2 className="section-title">Certifications</h2>
-            <div className={`certificate-gallery ${inView ? 'show' : ''}`}>
+            <motion.div
+                className={`certificate-gallery ${inView ? 'show' : ''}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: inView ? 1 : 0 }}
+                transition={{ duration: 1, ease: 'easeOut' }}
+            >
                 {certificateData.map((cert, index) => (
                     <motion.div
                         key={cert.id}
                         className="certificate"
-                        initial={{ opacity: 0, y: 50 }}
-                        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
-                        transition={{ delay: index * 0.1, type: 'spring', stiffness: 300 }}
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: inView ? 1 : 0, scale: inView ? 1 : 0.8 }}
+                        transition={{ delay: index * 0.2, type: 'spring', stiffness: 300 }}
                     >
                         <a href={cert.link} target="_blank" rel="noopener noreferrer">
                             <img src={cert.src} alt={cert.name} className="certificate-img" />
                         </a>
                     </motion.div>
                 ))}
-            </div>
+            </motion.div>
         </section>
     );
 };
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,54 +3,74 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import '../styles/Project.css';
 
-import projectOneImage from '../assets/babyecom.jpg';
-import projectTwoImage from '../assets/bill.jpg';
-import projectThreeImage from '../assets/taskmanagement.avif';
-import ProjectFourImage from '../assets/numberguess.png';
-import projectFiveImage from '../assets/blog.jpg';
+
+import projectOneImage from "../assets/babyecom.jpg";
+import projectTwoImage from "../assets/bill.jpg";
+import projectThreeImage from "../assets/taskmanagement.avif";
+import projectFourImage from "../assets/numberguess.png";
+import projectFiveImage from "../assets/blog.jpg";
+import projectSixImage from "../assets/view-3d-school-classroom.jpg";
+import projectSevenImage from "../assets/3d-representation-reselling-market.jpg";
+import projectEightImage from "../assets/3d-rendering-cartoon-house.jpg";
+import projectNineImage from "../assets/3246621.jpg";
 
 const projects = [
   {
     id: 1,
     title: "Baby Ecommerce",
-    description: "An advanced e-commerce platform for baby products using React and Node.js.",
-    techStack: "React, Node.js, Express, MongoDB, GCP, CI/CD, Docker",
     image: projectOneImage,
     link: "https://kidtryz.com/",
   },
   {
     id: 2,
     title: "Invoice Management",
-    description: "Built a serverless backend for efficient, scalable invoice management using AWS, enabling secure, on-demand generation and storage.",
-    techStack: "Node.js, AWS, Docker, DynamoDB, Swagger",
     image: projectTwoImage,
     link: "https://github.com/chinmayabeheragit/StoreWise",
   },
   {
     id: 3,
     title: "Task Management",
-    description: "A web-based task management system that enables users to create, view, update, and delete tasks.",
-    techStack: "MERN, JWT, AWS, Docker, CI/CD",
     image: projectThreeImage,
     link: "https://github.com/chinmayabeheragit/task-management-system-py/tree/main/task_management",
   },
   {
     id: 4,
     title: "Number Guess Game",
-    description: "This is a JavaScript-based Number Guess Game",
-    techStack: "HTML, CSS, JavaScript",
-    image: ProjectFourImage,
+    image: projectFourImage,
     link: "https://guessmynumber-game00.netlify.app/",
   },
   {
     id: 5,
     title: "AI Blog Generator",
-    description: "This is a Django-based AI Blog Generator application ",
-    techStack: "Python, Django, FastAPI, PostgreSQL, AssemblyAI, OpenAI, Docker, Kubernetes",
     image: projectFiveImage,
     link: "https://github.com/chinmayabeheragit/AI-Blog-Generator/",
   },
+  {
+    id: 6,
+    title: "Public School",
+    image: projectSixImage,
+    link: "https://kalyanpublicschool.org/",
+  },
+  {
+    id: 7,
+    title: "Ceya Export",
+    image: projectSevenImage,
+    link: "https://ceyaexport.com/",
+  },
+  {
+    id: 8,
+    title: "Utkal Property",
+    image: projectEightImage,
+    link: "https://utkalproperty.com/page/4/",
+  },
+  {
+    id: 9,
+    title: "AI Podcast Generator",
+    image: projectNineImage,
+    link: "https://github.com/chinmayabeheragit/ai_podcaster",
+  },
 ];
+
 
 const Project = () => {
   return (
@@ -80,7 +100,7 @@ const Project = () => {
                 </div>
                 <div className="project-info">
                   <h3 className="project-subtitle">{project.title}</h3>
-                  <p className="project-tech-stack"><strong>Tech Stack:</strong> {project.techStack}</p>
+                  {/* <p className="project-tech-stack"><strong>Tech Stack:</strong> {project.techStack}</p> */}
                 </div>
               </div>
             </a>
